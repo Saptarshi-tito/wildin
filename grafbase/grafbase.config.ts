20133ede01fe8667c4ea6038d1915971055f04d2
slade <story.tellers.free@gmail.com>
2024-02-03 03:42:06 +0530

    first commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,34 +1,31 @@
-import { g, graph, config } from '@grafbase/sdk'
+import { graph, config } from '@grafbase/sdk'
 
-// const g = graph.Standalone()
-// @ts-ignore
-// types are generated with the `type` method,
-// followed by the name and fields.
-const User = g.model('User', {
+// Welcome to Grafbase!
+//
+// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+
+const g = graph.Standalone()
+const Project = g.type('Project', {
+  title: g.string(),
+  description: g.string(), 
+  image: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
+  category: g.string()
+})
+
+const User = g.type('User', {
   name: g.string(),
   email: g.string(),
   avatarUrl: g.url(),
   description: g.string().optional(),
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(), 
-  projects: g.relation(() => Project).list().optional(),
+  projects: g.ref(Project),
 })
 
-  const Project = g.model('Project', {
-    title: g.string(),
-    description: g.string(), 
-    image: g.url(),
-    liveSiteUrl: g.url(), 
-    githubUrl: g.url(), 
-    category: g.string(),
-    createdBy: g.relation(() => User),
-  }).auth((rules) => {
-    rules.public().read()
-    rules.private().create().delete().update()
-  })
 
 
-// finally we export the default config
 export default config({
-  schema: g
+  graph: g,
 })
