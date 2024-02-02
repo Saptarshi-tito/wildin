f52a5b6af887c8654daf2fc5c9597eb9b62298f7
slade <story.tellers.free@gmail.com>
2024-02-03 03:50:03 +0530

    first commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,31 +1,41 @@
-import { graph, config } from '@grafbase/sdk'
+import { g, config, auth } from '@grafbase/sdk';
 
-// Welcome to Grafbase!
-//
-// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+// @ts-ignore
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(), 
+  projects: g.relation(() => Project).list().optional(),
+}).auth((rules) => {
+  rules.public().read()
+})
 
-const g = graph.Standalone()
-const Project = g.type('Project', {
-  title: g.string(),
+// @ts-ignore
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
   description: g.string(), 
   image: g.url(),
   liveSiteUrl: g.url(), 
   githubUrl: g.url(), 
-  category: g.string()
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+}).auth((rules) => {
+  rules.public().read()
+  rules.private().create().delete().update()
 })
 
-const User = g.type('User', {
-  name: g.string(),
-  email: g.string(),
-  avatarUrl: g.url(),
-  description: g.string().optional(),
-  githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
-  projects: g.ref(Project),
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret:  g.env('NEXTAUTH_SECRET')
 })
-
-
 
 export default config({
-  graph: g,
+  schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private()
+  },
 })
